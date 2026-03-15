da3150bbbcef619b775b8ced62a001b2989307bc
BATMAN <naikatharva34@gmail.com>
2026-03-16 01:29:49 +0530

    fix: more typescript errors (resizable components and zod errors)

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -69,7 +69,7 @@ const Auth = () => {
       if (error instanceof z.ZodError) {
         toast({
           title: "Validation error",
-          description: error.errors[0].message,
+          description: error.issues[0].message,
           variant: "destructive",
         });
       }
@@ -112,7 +112,7 @@ const Auth = () => {
       if (error instanceof z.ZodError) {
         toast({
           title: "Validation error",
-          description: error.errors[0].message,
+          description: error.issues[0].message,
           variant: "destructive",
         });
       }
